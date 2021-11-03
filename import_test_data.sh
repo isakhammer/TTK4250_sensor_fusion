1111dd9d0ab674cbb6cdf7815c4574cba3697179
isakhammer <isakhammer@gmail.com>
2021-11-03 15:24:28 +0100

    Added script to import test_data

--- import_test_data.sh
+++ import_test_data.sh
@@ -2,7 +2,8 @@
 unzip $( pwd )/slam_project/slam_handout.zip
 cp $( pwd )/slam_handout/tests/test_data.pickle $( pwd )/slam_project/tests/
 rm -Rf $( pwd )/slam_handout
-FILE=$( pwd )/slam_project/tests/
+
+FILE=$( pwd )/slam_project/tests/test_data.pickle
 
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
