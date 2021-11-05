e71019bb603fae8ecf54410a9b9109740e573cce
torsmi <55449319+torsmi@users.noreply.github.com>
2021-11-05 16:10:13 +0100

    Sim params good

--- eskf_project/eskf/tuning_sim.py
+++ eskf_project/eskf/tuning_sim.py
@@ -4,13 +4,13 @@ from datatypes.eskf_params import ESKFTuningParams
 from datatypes.eskf_states import NominalState, ErrorStateGauss
 
 tuning_params_sim = ESKFTuningParams(
-    accm_std=100.,
-    accm_bias_std=1.,
-    accm_bias_p=55.,
+    accm_std=0.07 * (1.0 / 60.0),       #m/s/sqrt(h) --> m/s/60*sqrt(sec)
+    accm_bias_std=0.05 * (9.81 / 100.0),            #mg --> 9.81/1000 m/s^2
+    accm_bias_p=0.000001,
 
-    gyro_std=100.,
-    gyro_bias_std=1.,
-    gyro_bias_p=1.,
+    gyro_std=0.15 * (np.pi / (180.0 * 60.0)),         #deg/sqrt(h) --> pi/180/60 rad/sqrt(sec)
+    gyro_bias_std=0.3 * (np.pi / (180.0 * 360.0)),     #deg/h --> pi/180/3600
+    gyro_bias_p=0.000001,
 
     gnss_std_ne=0.0921,
     gnss_std_d=0.2693)
@@ -18,7 +18,7 @@ tuning_params_sim = ESKFTuningParams(
 x_nom_init_sim = NominalState(
     np.array([0.2, 0.0, -5.0]),  # position
     np.array([20.0, 0.0275, 0.1133]),  # velocity
-    RotationQuaterion.from_euler([1.0, 0.0, 0.0]),  # orientation
+    RotationQuaterion.from_euler([0.0, 0.0, 0.0]),  # orientation
     np.array([0.0, 0.0, 0.0]),  # accelerometer bias
     np.array([0.0, 0.0, 0.0]),  # gyro bias
     ts=0.)
@@ -28,5 +28,5 @@ init_std_sim = np.repeat(repeats=3,  # repeat each element 3 times
                             1.,  # velocity
                             np.deg2rad(1),  # angle vector
                             1.,  # accelerometer bias
-                            1.])  # gyro bias
+                            0.01])  # gyro bias
 x_err_init_sim = ErrorStateGauss(np.zeros(15), np.diag(init_std_sim**2), 0.)
